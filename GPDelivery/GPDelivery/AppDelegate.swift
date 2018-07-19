622fd06601f461d13b9cdb07384ab3fff4a83d43
Glenn Posadas <admin@glennvon.com>
2018-07-19 18:00:01 +0800

    - Added project files.

---
+++ GPDelivery/GPDelivery/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GPDelivery
+//
+//  Created by Glenn Von C. Posadas on 19/07/2018.
+//  Copyright © 2018 Glenn Von C. Posadas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        
+        return true
+    }
+}
+
